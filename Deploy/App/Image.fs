7168051cad0480c34fa7538f1cdee582cfd93e0f
zh6335901 <zh6335901@hotmail.com>
2023-05-21 22:27:20 +0800

    Add docker building

---
+++ Deploy/App/Image.fs
@@ -0,0 +1,36 @@
+﻿module Image
+
+open Pulumi.Docker
+open Pulumi.Docker.Inputs
+open Pulumi.FSharp
+
+type DockerRegistry =
+    { Server: string
+      Username: string
+      Password: string }
+
+let build registry =
+    let image =
+        Image(
+            "MyProductImage",
+            ImageArgs(
+                ImageName = input "pulumi-poc/MyProduct",
+                Build =
+                    input (
+                        DockerBuildArgs(
+                            Context = "./Source/MyProduct.Api",
+                            Dockerfile = "./Source/MyProduct.Api/Dockerfile"
+                        )
+                    ),
+                Registry =
+                    input (
+                        RegistryArgs(
+                            Server = registry.Server,
+                            Username = registry.Username,
+                            Password = registry.Password
+                        )
+                    )
+            )
+        )
+
+    image
